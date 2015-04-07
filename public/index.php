23885365f2d4c8a8f51d81361a4666a298360f3f
Tom Hudson <mail@tomnomnom.com>
2015-04-07 07:27:35 -0400

    Initial

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>edutom.co.uk</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+    <header>
+      <h1>edutom.co.uk</h1>
+    </header>
+
+    <section class="main">
+      <ul>
+        <li><a href="/clocks/">Clocks</a></li>
+      </ul>
+    </section>
+
+    <script>
+    </script>
+    
+  </body>
+</html>
